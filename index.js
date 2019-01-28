9b716bc744a4bd4d946c95259032092d146cf1cb
Ketan <gondaliya900@gmail.com>
2019-01-28 16:01:24 +0530

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express')
- 
+var bodyParser = require("body-parser")
+
 
 const app = express(); 
 app.set("views", "./views");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
 app.get('/',(req,res) =>{
-
     res.render("home")
-
-    // const HummusRecipe = require('hummus-recipe');
-    // const pdfDoc = new HummusRecipe('Certificate.pdf', 'output.pdf');
-    // pdfDoc
-    //     // edit 1st page
-    //     .editPage(1)
-    //     .text('Ketan Gondaliya', 500, 400)
-
-    //     .endPage()
-    //     // end and save
-    //     .endPDF();
-    //     res.send("PDF saved");
 })
 
 app.post('/generate',(req,res) => {
     // res.send(req.body.nameone)
-    console.log(req.body)
-    console.log(req.body.nameone)
+    // console.log(req.body)
+    let name123 = req.body.name;
+    const HummusRecipe = require('hummus-recipe');
+    const pdfDoc = new HummusRecipe('Certificate.pdf', 'output.pdf');
+    pdfDoc
+        // edit 1st page
+        .editPage(1)
+        .text(name123, 415, 380)
+
+        .endPage()
+        // end and save
+        .endPDF();
+    res.send("PDF saved");
+
 })
 
 
