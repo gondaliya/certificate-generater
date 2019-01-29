a48de2705dfdd9635f45817e064a241b273a13a1
Ketan <gondaliya900@gmail.com>
2019-01-29 21:56:37 +0530

    css edited-2

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 const express = require('express')
-var bodyParser = require("body-parser")
+var bodyParser = require('body-parser')
+const HummusRecipe = require('hummus-recipe')
 const fs = require('fs')
+const path = require('path')
 
-const app = express(); 
-app.set("views", "./views");
+// create a stdout and file logger
+const log = require('simple-node-logger').createSimpleLogger('project.log');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+const app = express()
+app.set('views', './views')
+
+//used for applying css to ejs file
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
-app.set('view engine', 'ejs');
-app.get('/',(req,res) =>{
-    res.render("home")
+  })
+)
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
-app.post('/generate',(req,res) => {
-    // res.send(req.body.nameone)
-    // console.log(req.body)
-    let name123 = req.body.name;
-    const HummusRecipe = require('hummus-recipe');
-    const pdfDoc = new HummusRecipe('Certificate.pdf', 'output.pdf');
-    pdfDoc
-        // edit 1st page
-        .editPage(1)
-        .text(name123, 415, 380)
+app.post('/generate', (req, res) => {
+  // res.send(req.body.nameone)
+  // console.log(req.body)
+  let name123 = req.body.name
 
-        .endPage()
-        // end and save
-        .endPDF();
-    var data = fs.readFileSync('./output.pdf');
-    res.contentType("application/pdf");
-    res.send(data);
-    // res.send("PDF saved");
+  //logging name to project.log file
+  log.info('name is ', name123, "  ", new Date().toJSON())
 
+  const pdfDoc = new HummusRecipe('Certificate.pdf', 'output.pdf')
+  pdfDoc
+    // edit 1st page
+    .editPage(1)
+    .text(name123, 415, 380)
+
+    .endPage()
+    // end and save
+    .endPDF()
+  var data = fs.readFileSync('./output.pdf')
+  res.contentType('application/pdf')
+  res.send(data)
+  // res.send("PDF saved");
 })
 
-
-app.listen(5001,()=>{
-    console.log("started")
+app.listen(5001, () => {
+  console.log('started')
 })
---
+++ public/js/effect.js
@@ -0,0 +1,14 @@
+// console.log("hello there")
+// const element = document.querySelector('.container');
+// // element.classList.add('animated', 'bounceOutLeft')
+// window.alert("its working")
+// alert("heynode");
+
+var a= document.getElementById("name");
+function Validator() {
+    if (a.innerText = "") {
+        alert("Enter your Name");
+        return false;
+    }
+
+}
